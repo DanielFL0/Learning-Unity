1f9fd843b9f066a2396e20fa71b5e93b58256ae9
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-21 15:05:10 -0500

    Add 1-First-Steps and 2-Terminal-Hacker

---
+++ 2.-Terminal-Hacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+
+    // Game States
+    int level;
+    enum Screen { MainMenu, Password, Win};
+    Screen currentScreen = Screen.MainMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu()
+    {
+        currentScreen = Screen.MainMenu;
+        string message = "Enter one of the following options" +
+            "\n1- Hack public library." +
+            "\n2- Hack police station." +
+            "\n3- Hack NASA.";
+        Terminal.ClearScreen();
+        Terminal.WriteLine(message);
+    }
+
+    void OnUserInput(string input)
+    {
+        if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+        else if (currentScreen == Screen.MainMenu)
+        {
+            RunMainMenu(input);
+        }
+    }
+
+    void RunMainMenu(string input)
+    {
+        if (input == "1")
+        {
+            level = 1;
+            StartGame();
+        }
+        else if (input == "2")
+        {
+            level = 2;
+            StartGame();
+        }
+        else if (input == "3")
+        {
+            level = 3;
+            StartGame();
+        }
+        else
+        {
+            print(input);
+        }
+    }
+
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("Level selected is: " + level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
