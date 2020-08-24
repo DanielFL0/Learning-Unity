f92699ad63da03c38de4e5b390f47b53267db5b0
DanielFl0 <jose.saldanafrs@uanl.edu.mx>
2020-08-23 20:01:48 -0500

    Modify 3-Project-Boost

---
+++ 3-Project-Boost/Assets/Assets/Scripts/Rocket.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    [SerializeField] float mainThrust = 300f;
+    [SerializeField] float rcsThrust = 25;
+    public Rigidbody rigidbody;
+    public AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.mass = 10f;
+        rigidbody.drag = 0.2f;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.volume = 0.3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space)) // Can trust while rotating.
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+        }
+        else
+        {
+            audioSource.Stop();
+        }
+    }
+
+    void Rotate()
+    {
+        rigidbody.freezeRotation = true;
+        float rotationSpeed = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationSpeed);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationSpeed);
+        }
+        rigidbody.freezeRotation = false;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+
+                break;
+
+            default:
+
+                break;
+        }
+    }
+
+}
